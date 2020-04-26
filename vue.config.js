04f38b854396ffc03068bda7a0858374e46e5832
Maximiliano Vicente <vicente.maximilianoh@gmail.com>
2020-04-26 20:31:21 -0300

    Commented out PWA settings

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/scorepad/" : "/",
-  pwa: {
-    name: "Score Pad",
-    themeColor: "#4DBA87",
-    msTileColor: "#000000",
-    appleMobileWebAppCapable: "yes",
-    appleMobileWebAppStatusBarStyle: "black"
+  publicPath: process.env.NODE_ENV === "production" ? "/dist/" : "/"
+  // pwa: {
+  //   name: "Score Pad",
+  //   themeColor: "#4DBA87",
+  //   msTileColor: "#000000",
+  //   appleMobileWebAppCapable: "yes",
+  //   appleMobileWebAppStatusBarStyle: "black",
 
-    // configure the workbox plugin
-    // workboxPluginMode: "InjectManifest",
-    // workboxOptions: {
-    // swSrc is required in InjectManifest mode.
-    // swSrc: "dev/sw.js",
-    // ...other Workbox options...
-    // },
-  }
+  // configure the workbox plugin
+  // workboxPluginMode: "InjectManifest",
+  // workboxOptions: {
+  // swSrc is required in InjectManifest mode.
+  // swSrc: "dev/sw.js",
+  // ...other Workbox options...
+  // },
+  // },
 };
