8a908b67b4dc4dc3a2ad07211c86143ffa727ae4
Maximiliano Vicente <vicente.maximilianoh@gmail.com>
2020-04-26 20:21:15 -0300

    Added deployment config

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/scorepad/" : "/",
+  pwa: {
+    name: "Score Pad",
+    themeColor: "#4DBA87",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black"
+
+    // configure the workbox plugin
+    // workboxPluginMode: "InjectManifest",
+    // workboxOptions: {
+    // swSrc is required in InjectManifest mode.
+    // swSrc: "dev/sw.js",
+    // ...other Workbox options...
+    // },
+  }
+};
