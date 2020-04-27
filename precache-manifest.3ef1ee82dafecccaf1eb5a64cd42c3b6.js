a1f1d8f007c9f92befc2e8d67136c9817c1b2fdf
Maximiliano Vicente <vicente.maximilianoh@gmail.com>
2020-04-26 23:55:43 -0300

    deploy

---
+++ precache-manifest.3ef1ee82dafecccaf1eb5a64cd42c3b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f1309b56e8aabdaa152",
+    "url": "/scorepad/css/app.5674c0f1.css"
+  },
+  {
+    "revision": "3a8faa1a4927a072d1f5de31cdc03621",
+    "url": "/scorepad/index.html"
+  },
+  {
+    "revision": "9e91e76ded6d4d271772",
+    "url": "/scorepad/js/about.392b6fec.js"
+  },
+  {
+    "revision": "2f1309b56e8aabdaa152",
+    "url": "/scorepad/js/app.162c0d53.js"
+  },
+  {
+    "revision": "39013f62e6c5c945e05a",
+    "url": "/scorepad/js/chunk-vendors.a457c039.js"
+  },
+  {
+    "revision": "0bdaea3212c79b16f56f04ff31858297",
+    "url": "/scorepad/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/scorepad/robots.txt"
+  }
+]);
